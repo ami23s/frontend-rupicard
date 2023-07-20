eea87d9212f2029fd45db0cbe5f3505e38e234ad
ami23s <ami23071999@gmail.com>
2023-07-20 14:57:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { form } from "./form"
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  
+  const handleNameChange = (event) => {
+    setName(event.target.value)
+  }
+
+  const handlePhoneChange = (event) => {
+    setPhone(event.target.value)
+  }
+
+  const onSubmit = () => {
+    let ob = {
+      "name": name,
+      "phone": phone
+    }
+
+    console.log("hello")
+  fetch("http://localhost:5000/api/data", {
+    method: "POST",  
+    headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(ob)
+    })
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>name </label>   
+        <input type="text"
+        id="name"
+        name="name"
+        onChange={handleNameChange}
+        value={name}></input> 
+        <label>Phone</label> 
+        <input type="text"
+        id="phone"
+        name="phone"
+        onChange={handlePhoneChange}
+        value={phone}></input> 
+        <button style={{color:"red", padding: "5px"}} type="submit" value="Submit" onClick={()=>{onSubmit()}}>Submit</button>
     </div>
   );
 }
---
+++ src/form.js
@@ -0,0 +1,13 @@
+const form = () => {
+    return(
+        <div>
+            <label>User Id: </label>   
+            <input type="text"></input> 
+            <label>Passzord:</label> 
+            <input type="password"> </input> 
+            <input type="submit" value="Submit"></input>  
+        </div>
+    )
+}
+
+export default form
